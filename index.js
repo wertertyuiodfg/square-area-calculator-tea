41a60f912e46f90e1efa0caef0239b23dbe3a67a
dfghjkcvbnmnbvcx <162674888+wertertyuiodfg@users.noreply.github.com>
2024-03-16 22:10:26 +0700

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+// index.js
+
+function calculateSquareArea(sideLength) {
+    return sideLength * sideLength;
+  }
+  
+  const sideLength = 5;
+  const area = calculateSquareArea(sideLength);
+  console.log(`Area of the square with side length ${sideLength} is: ${area}`);
+  
